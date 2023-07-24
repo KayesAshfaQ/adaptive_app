2b3ba8db55336c8aeaf5d9ba850d769578eb523c
Kayes Ashfaq <asfak7074@gmail.com>
2023-07-24 22:50:24 +0600

    ResizableScreen added, HomeScreen removed

---
+++ lib/screens/resizable_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ResizableScreen extends StatelessWidget {
+  const ResizableScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
