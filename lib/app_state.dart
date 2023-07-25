b31b60c2aca6e6c686160d98c24ad1de5b6f190c
Kayes Ashfaq <asfak7074@gmail.com>
2023-07-25 22:38:27 +0600

    app_state.dart code explaination comments added

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -18,15 +18,18 @@ class FlutterDevPlaylists extends ChangeNotifier {
     required String flutterDevAccountId,
     required String youTubeApiKey,
   }) : _flutterDevAccountId = flutterDevAccountId {
+    // The YouTubeApi initialized with an instance of _ApiKeyClient, which is a custom HTTP client
     _api = YouTubeApi(
       _ApiKeyClient(
         client: http.Client(),
         key: youTubeApiKey,
       ),
     );
+
     _loadPlayLists();
   }
 
+  // It fetches the list of playlists from the YouTube API and stores them in the _playlists list.
   Future<void> _loadPlayLists() async {
     String? nextPageToken;
     _playlists.clear();
@@ -47,6 +50,8 @@ class FlutterDevPlaylists extends ChangeNotifier {
     } while (nextPageToken != null);
   }
 
+  // This method is responsible for retrieving the data from _playlistItems map
+  // by using the playlistId and returns an unmodifiable list of PlaylistItem objects.
   List<PlaylistItem> playlistItems({required String playlistId}) {
     if (!_playlistItems.containsKey(playlistId)) {
       _playlistItems[playlistId] = [];
@@ -55,6 +60,8 @@ class FlutterDevPlaylists extends ChangeNotifier {
     return UnmodifiableListView(_playlistItems[playlistId]!);
   }
 
+  // It fetches the list of playlist items (videos) from the YouTube API
+  // and stores them in the _playlistItems map.
   Future<void> _retrievePlaylist(String playlistId) async {
     String? nextPageToken;
     do {
@@ -80,17 +87,23 @@ class _ApiKeyClient extends http.BaseClient {
   final String key;
   final http.Client client;
 
+  /*
+    This code is to add the API key to the query parameters of the HTTP request before it is sent to the YouTube API. 
+    This is necessary to authenticate the request and ensure that it is authorized to access the requested resources.
+  */
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) {
-    print('original url::: ${request.url}');
-
+    // replaces the queryParameters of the request URL with a new map that creates
+    // a new map that includes all the existing query parameters of the URL
+    // and adds a new key-value pair for the API key.
     final url = request.url.replace(queryParameters: <String, List<String>>{
       ...request.url.queryParametersAll,
       'key': [key]
     });
 
-    print('modified url::: $url');
-
+    // The modified URL is then used to create a new http.Request instance, which is passed to the send method
+    // of the underlying HTTP client. The send method returns a Future that resolves to an instance of
+    // http.StreamedResponse, which represents the HTTP response received from the server.
     return client.send(http.Request(request.method, url));
   }
 }
